99e21467ff177cca5b153264267e7db1b5bbead2
Xin chao mn <mactrung116@gmail.com>
2025-05-06 11:22:22 +0700

    update 6/5 11:22

--- TokenSample/Models/HocSinhModel.cs
+++ TokenSample/Models/HocSinhModel.cs
@@ -1,9 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TokenSample.Data;
 
 namespace TokenSample.Models
 {
     public class HocSinhModel
     {
+        [Key]
+        public int Id { get; set; }
         [Required]
         [MaxLength(100)]
         public string? Name { get; set; }
@@ -11,5 +15,8 @@ namespace TokenSample.Models
         [MaxLength(100)]
         public string? Address { get; set; }
         public int? Age { get; set; }
+        public int Malop { get; set; }
+        [ForeignKey("Malop")]
+        public LopHoc? LopHoc { get; set; }
     }
 }
--- TokenSample/Models/LopHocModel.cs
+++ TokenSample/Models/LopHocModel.cs
@@ -4,6 +4,8 @@ namespace TokenSample.Models
 {
     public class LopHocModel
     {
+        [Key]
+        public int MaLop { get; set; }
         [Required]
         [MaxLength(100)]
         public string? TenLop { get; set; }
